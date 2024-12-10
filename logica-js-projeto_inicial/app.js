26660adbec1192aa7dcb73a27e03447246c33078
luiz675 <luizadrianosenac@gmail.com>
2024-12-10 00:28:22 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,26 @@
+alert ('Boas vindas ao jogo do numero secreto');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    if(chute == numeroSecreto){
+        break;
+    } else {
+        if(chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute} `);
+        } else {
+            alert(`O número secreto é maior que ${chute} `); 
+        }
+        tentativas++;
+    }
+
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    alert(`Isso ai! você descobriu o secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.` );
+
+
